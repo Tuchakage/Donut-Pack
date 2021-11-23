8edcced4b5b9c68686515b44de6bb121ca68e934
Tuchakage <62402636+Tuchakage@users.noreply.github.com>
2021-11-23 01:21:54 +0000

    Got rid of only set amount of items spawning, You lose ingredients if you put wrong ingredients into a full container

--- Unity Test Projects/Mobile Test/Assets/Scripts/Container.cs
+++ Unity Test Projects/Mobile Test/Assets/Scripts/Container.cs
@@ -7,8 +7,7 @@ public class Container : MonoBehaviour
     //For changing the sprites for the containers
     public SpriteRenderer spriteRenderer;
     public Sprite[] bin;
-    public Sprite halffull;
-    public Sprite full;
+    public Sprite[] containerFill; //Sprites for how full the Containers will show as
 
     public DragController dc;
     GameManager gm;
@@ -21,7 +20,6 @@ public class Container : MonoBehaviour
     [SerializeField]
     private int amntgoodmilk, amntgoodyeast,amntsalt,amntsugar,amntflour;
     [SerializeField]
-    private bool isItemWrong; //Check if the item put into the container was the wrong one
     private bool isComplete = false; // Checks if the container is full
     // Start is called before the first frame update
     void Start()
@@ -42,27 +40,30 @@ public class Container : MonoBehaviour
             {
                 Debug.Log("Correct Item");
                 Destroy(col.gameObject);
-                //If you put the right object in the right container you get more time
-                gm.addTime(addedtime, this.transform.position);
-                //Increment this variable
-                amntgoodmilk++;
-                //Item that was put into this container was right
-                isItemWrong = false;
-                //If the milk container is half way full
-                if (amntgoodmilk == items.maxAmntOfItem / 2)
+                //If the container is not full yet
+                if (amntgoodmilk < items.maxAmntOfItem) 
                 {
-                    //Change into the Sprite with the measuring cup half full
-                    spriteRenderer.sprite = halffull;
+                    //If you put the right object in the right container you get more time
+                    gm.addTime(addedtime, this.transform.position);
+                    //Increment this variable
+                    amntgoodmilk++;
+                    //If the milk container is half way full
+                    if (amntgoodmilk == items.maxAmntOfItem / 2)
+                    {
+                        //Change into the Sprite with the measuring cup half full
+                        spriteRenderer.sprite = containerFill[1];
+                    }
+                    else if (amntgoodmilk == items.maxAmntOfItem) //If the container is full
+                    {
+                        //Change into the Sprite with the measuring cup full
+                        spriteRenderer.sprite = containerFill[2];
+                        //The container is full
+                        isComplete = true;
+                        //Tell the Game Manager that a container has been completed
+                        gm.amntContainersComplete++;
+                    }
                 }
-                else if (amntgoodmilk == items.maxAmntOfItem) //If the container is full
-                {
-                    //Change into the Sprite with the measuring cup full
-                    spriteRenderer.sprite = full;
-                    //The container is full
-                    isComplete = true;
-                    //Tell the Game Manager that a container has been completed
-                    gm.amntContainersComplete++;
-                }
+
             }
             else if (col.gameObject.tag != "GoodMilk" && !dc.isDragActive) //If the gameobject is colliding with the wrong container
             {
@@ -70,8 +71,22 @@ public class Container : MonoBehaviour
                 Destroy(col.gameObject);
                 //If you put the wrong object into the wrong container then you lose time
                 gm.loseTime(losttime, this.transform.position);
-                //Item that was put into this container was wrong
-                isItemWrong = true;
+                //If there is already ingredients in this container and then a wrong item is put into the container
+                if (amntgoodmilk > 0) 
+                {
+                    //Decrease the amount of milk
+                    amntgoodmilk--;
+                    if (amntgoodmilk == items.maxAmntOfItem / 2) 
+                    {
+                        //Change the sprite to being half full again
+                        spriteRenderer.sprite = containerFill[1];
+                    }
+                    else if (amntgoodmilk < 1) //If the amount in this container is back to 0
+                    {
+                        //Make the Measuring Cup Sprite Empty again
+                        spriteRenderer.sprite = containerFill[0];
+                    }
+                }
             }
         }
         else if (this.gameObject.name == "Yeast Bowl") //If the gameObject this script is on is the Yeast bowl
@@ -81,28 +96,29 @@ public class Container : MonoBehaviour
             {
                 Debug.Log("Correct Item");
                 Destroy(col.gameObject);
-                //If you put the right object in the right container you get more time
-                gm.addTime(addedtime, this.transform.position);
-                //Item that was put into this container was right
-                isItemWrong = false;
-                //Increment this variable
-                amntgoodyeast++;
-                //If the milk container is half way full
-                if (amntgoodyeast == items.maxAmntOfItem / 2)
+                //If the container is not full yet
+                if (amntgoodyeast < items.maxAmntOfItem)
                 {
-                    //Change into the Sprite with the Yeast bowl half full
-                    spriteRenderer.sprite = halffull;
+                    //If you put the right object in the right container you get more time
+                    gm.addTime(addedtime, this.transform.position);
+                    //Increment this variable
+                    amntgoodyeast++;
+                    //If the container is half way full
+                    if (amntgoodyeast == items.maxAmntOfItem / 2)
+                    {
+                        //Change into the Sprite with the Yeast bowl half full
+                        spriteRenderer.sprite = containerFill[1];
+                    }
+                    else if (amntgoodyeast == items.maxAmntOfItem) //If the container is full
+                    {
+                        //Change into the Sprite with the Yeast bowl full
+                        spriteRenderer.sprite = containerFill[2];
+                        //Set this container to being complete
+                        isComplete = true;
+                        //Tell the Game Manager that a container has been completed
+                        gm.amntContainersComplete++;
+                    }
                 }
-                else if (amntgoodyeast == items.maxAmntOfItem) //If the container is full
-                {
-                    //Change into the Sprite with the Yeast bowl full
-                    spriteRenderer.sprite = full;
-                    //Set this container to being complete
-                    isComplete = true;
-                    //Tell the Game Manager that a container has been completed
-                    gm.amntContainersComplete++;
-                }
-
 
             }
             else if (col.gameObject.tag != "GoodYeast" && !dc.isDragActive) //If the gameobject is colliding with the wrong container
@@ -111,8 +127,22 @@ public class Container : MonoBehaviour
                 Destroy(col.gameObject);
                 //If you put the wrong object into the wrong container then you lose time
                 gm.loseTime(losttime, this.transform.position);
-                //Item that was put into this container was wrong
-                isItemWrong = true;
+                //If there is already ingredients in this container and then a wrong item is put into the container
+                if (amntgoodyeast > 0)
+                {
+                    //Decrease the amount of Yeast
+                    amntgoodyeast--;
+                    if (amntgoodyeast == items.maxAmntOfItem / 2)
+                    {
+                        //Change the sprite to being half full again
+                        spriteRenderer.sprite = containerFill[1];
+                    }
+                    else if (amntgoodyeast < 1) //If the amount in this container is back to 0
+                    {
+                        //Make the Measuring Cup Sprite Empty again
+                        spriteRenderer.sprite = containerFill[0];
+                    }
+                }
             }
         }
         else if (this.gameObject.name == "Salt Bowl") //If the gameObject this script is on is the Salt bowl
@@ -122,20 +152,23 @@ public class Container : MonoBehaviour
             {
                 Debug.Log("Correct Item");
                 Destroy(col.gameObject);
-                //If you put the right object in the right container you get more time
-                gm.addTime(addedtime, this.transform.position);
-                //Item that was put into this container was right
-                isItemWrong = false;
-                //Increment this variable 
-                amntsalt++;
-                //If Contrainer is full
-                if (amntsalt == items.maxAmntOfItem)
+                //If the container is not full yet
+                if (amntsalt < items.maxAmntOfItem) 
                 {
-                    //Set this container to being complete
-                    isComplete = true;
-                    //Tell the Game Manager that a container has been completed
-                    gm.amntContainersComplete++;
+                    //If you put the right object in the right container you get more time
+                    gm.addTime(addedtime, this.transform.position);
+                    //Increment this variable 
+                    amntsalt++;
+                    //If Contrainer is full
+                    if (amntsalt == items.maxAmntOfItem)
+                    {
+                        //Set this container to being complete
+                        isComplete = true;
+                        //Tell the Game Manager that a container has been completed
+                        gm.amntContainersComplete++;
+                    }
                 }
+
             }
             else if (col.gameObject.tag != "GoodSalt" && !dc.isDragActive) //If the gameobject is colliding with the wrong container
             {
@@ -143,8 +176,22 @@ public class Container : MonoBehaviour
                 Destroy(col.gameObject);
                 //If you put the wrong object into the wrong container then you lose time
                 gm.loseTime(losttime, this.transform.position);
-                //Item that was put into this container was wrong
-                isItemWrong = true;
+                //If there is already ingredients in this container and then a wrong item is put into the container
+                if (amntsalt > 0)
+                {
+                    //Decrease the amount of milk
+                    amntsalt--;
+                    if (amntsalt == items.maxAmntOfItem / 2)
+                    {
+                        //Change the sprite to being half full again
+                        //spriteRenderer.sprite = containerFill[1];
+                    }
+                    else if (amntsalt < 1) //If the amount in this container is back to 0
+                    {
+                        //Make the Measuring Cup Sprite Empty again
+                        //spriteRenderer.sprite = containerFill[0];
+                    }
+                }
             }
         }
 
@@ -155,20 +202,23 @@ public class Container : MonoBehaviour
             {
                 Debug.Log("Correct Item");
                 Destroy(col.gameObject);
-                //If you put the right object in the right container you get more time
-                gm.addTime(addedtime, this.transform.position);
-                //Item that was put into this container was right
-                isItemWrong = false;
-                //Increment this variable 
-                amntsugar++;
-                //If Contrainer is full
-                if (amntsugar == items.maxAmntOfItem)
+                //If the container is not full yet
+                if (amntsugar < items.maxAmntOfItem) 
                 {
-                    //Set this container to being complete
-                    isComplete = true;
-                    //Tell the Game Manager that a container has been completed
-                    gm.amntContainersComplete++;
+                    //If you put the right object in the right container you get more time
+                    gm.addTime(addedtime, this.transform.position);
+                    //Increment this variable 
+                    amntsugar++;
+                    //If Contrainer is full
+                    if (amntsugar == items.maxAmntOfItem)
+                    {
+                        //Set this container to being complete
+                        isComplete = true;
+                        //Tell the Game Manager that a container has been completed
+                        gm.amntContainersComplete++;
+                    }
                 }
+
             }
             else if (col.gameObject.tag != "GoodSugar" && !dc.isDragActive) //If the gameobject is colliding with the wrong container
             {
@@ -176,8 +226,22 @@ public class Container : MonoBehaviour
                 Destroy(col.gameObject);
                 //If you put the wrong object into the wrong container then you lose time
                 gm.loseTime(losttime, this.transform.position);
-                //Item that was put into this container was wrong
-                isItemWrong = true;
+                //If there is already ingredients in this container and then a wrong item is put into the container
+                if (amntsugar > 0)
+                {
+                    //Decrease the amount of milk
+                    amntsugar--;
+                    if (amntsugar == items.maxAmntOfItem / 2)
+                    {
+                        //Change the sprite to being half full again
+                        //spriteRenderer.sprite = containerFill[1];
+                    }
+                    else if (amntsugar < 1) //If the amount in this container is back to 0
+                    {
+                        //Make the Measuring Cup Sprite Empty again
+                        //spriteRenderer.sprite = containerFill[0];
+                    }
+                }
             }
         }
 
@@ -188,20 +252,23 @@ public class Container : MonoBehaviour
             {
                 Debug.Log("Correct Item");
                 Destroy(col.gameObject);
-                //If you put the right object in the right container you get more time
-                gm.addTime(addedtime, this.transform.position);
-                //Item that was put into this container was right
-                isItemWrong = false;
-                //Increment this variable 
-                amntflour++;
-                //If Contrainer is full
-                if (amntflour == items.maxAmntOfItem)
+                //If the container is not full yet
+                if (amntflour < items.maxAmntOfItem)
                 {
-                    //Set this container to being complete
-                    isComplete = true;
-                    //Tell the Game Manager that a container has been completed
-                    gm.amntContainersComplete++;
+                    //If you put the right object in the right container you get more time
+                    gm.addTime(addedtime, this.transform.position);
+                    //Increment this variable 
+                    amntflour++;
+                    //If Contrainer is full
+                    if (amntflour == items.maxAmntOfItem)
+                    {
+                        //Set this container to being complete
+                        isComplete = true;
+                        //Tell the Game Manager that a container has been completed
+                        gm.amntContainersComplete++;
+                    }
                 }
+
             }
             else if (col.gameObject.tag != "GoodFlour" && !dc.isDragActive) //If the gameobject is colliding with the wrong container
             {
@@ -209,8 +276,21 @@ public class Container : MonoBehaviour
                 Destroy(col.gameObject);
                 //If you put the wrong object into the wrong container then you lose time
                 gm.loseTime(losttime, this.transform.position);
-                //Item that was put into this container was wrong
-                isItemWrong = true;
+                if (amntflour > 0)
+                {
+                    //Decrease the amount of milk
+                    amntflour--;
+                    if (amntflour == items.maxAmntOfItem / 2)
+                    {
+                        //Change the sprite to being half full again
+                        //spriteRenderer.sprite = containerFill[1];
+                    }
+                    else if (amntflour < 1) //If the amount in this container is back to 0
+                    {
+                        //Make the Measuring Cup Sprite Empty again
+                        //spriteRenderer.sprite = containerFill[0];
+                    }
+                }
             }
         }
 
@@ -222,16 +302,12 @@ public class Container : MonoBehaviour
                 Destroy(col.gameObject);
                 //If you put the right object in the right container you get more time
                 gm.addTime(addedtime, this.transform.position);
-                //Item that was put into this container was right
-                isItemWrong = false;
             }
             else if (col.gameObject.tag != "BadItems" && !dc.isDragActive) //If the gameobject is colliding with the wrong container
             {
                 Destroy(col.gameObject);
                 //If you put the wrong object into the wrong container then you lose time
                 gm.loseTime(losttime, this.transform.position);
-                //Item that was put into this container was wrong
-                isItemWrong = true;
             }
 
             if (dc.isDragActive) //If the player is hovering an item over the bin
@@ -243,24 +319,8 @@ public class Container : MonoBehaviour
         else if (this.gameObject.name == "Border") // If an item hits the borders of the game
         {
             Destroy(col.gameObject);
-            //Set isItemWrong variable to true which means the item can be respawned
-            isItemWrong = true;
         }
 
-
-
-        //If the item has been dropped into the container
-        if (!dc.isDragActive) 
-        {
-            //Check if the item that was dropped in was the wrong one
-            if (isItemWrong) 
-            {
-                //Make sure the item can be respawned
-                items.IncreaseSpawn(col.gameObject);
-                Debug.Log(col.gameObject.name + "Dropped into " + this.gameObject.name+ " is wrong!");
-            }
-
-        }
     }
 
     private void OnTriggerExit2D(Collider2D col)
