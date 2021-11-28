612c9ea2714c53856a0bb10e02c39a65e9d87768
Tuchakage <62402636+Tuchakage@users.noreply.github.com>
2021-11-28 14:12:56 +0000

    Collisions for Sprinkles made better and a border that destroys any sprinkles that touches it

--- Unity Test Projects/Mobile Test/Assets/Scripts/SprinklesCollision.cs
+++ Unity Test Projects/Mobile Test/Assets/Scripts/SprinklesCollision.cs
@@ -25,8 +25,8 @@ public class SprinklesCollision : MonoBehaviour
         ParticleSystem.MainModule psm = col.gameObject.GetComponent<ParticleSystem>().main;
         //Disable the gravity
         psm.gravityModifier = 0;
-        //if there is less than 1 sprinkles in the spot, then sprinkles can stick onto that space of the Donut
-        if (numofParticles < 1)
+        //if there is less than 6 sprinkles in the spot, then sprinkles can stick onto that space of the Donut
+        if (numofParticles < 6)
         {
             //Puts the Sprinkle Particles that collided with the object into the Sprinkles variable
             Sprinkles = col.gameObject;
@@ -43,6 +43,12 @@ public class SprinklesCollision : MonoBehaviour
         {
             //Disable the particles so no more sprinkles can go on it
             gameObject.GetComponent<CircleCollider2D>().enabled= false;
+        }
+
+        //If the Sprinkles touch the border then delete them
+        if (this.gameObject.name == "Border") 
+        {
+            Destroy(col.gameObject);
         }
     }
 }
