424baadd75a73cca4b0105cddce979d882941189
Tuchakage <62402636+Tuchakage@users.noreply.github.com>
2021-12-04 15:22:57 +0000

    Got rid of some useless code in "ChangeSprinkleColour" Script

--- Unity Test Projects/Mobile Test/Assets/Scripts/ChangeSprinkleColour.cs
+++ Unity Test Projects/Mobile Test/Assets/Scripts/ChangeSprinkleColour.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class ChangeSprinkleColour : MonoBehaviour
 {
-    Dictionary<int, Material> sprinklecolour = null;
     public List <Material> materials = new List<Material>(); //So we can add new Colours by making the material and adding it to the list
     public List<Sprite> sprinkleShape = new List<Sprite>(); //So we can add new shapes for the Sprinkles
     private int randomColour = 0;
     private int randomShape = 0;
     // Start is called before the first frame update
     void Start()
-    {
-
-        //Create a dictionary for Sprinkle Colours
-        sprinklecolour = new Dictionary<int, Material>();
-        AddColoursToDictionary();
+    {;
         //Pick a random Colour and Shape
         RandomSprinkle();
         //Find the particle System
@@ -28,7 +23,7 @@ public class ChangeSprinkleColour : MonoBehaviour
         //Find the Particle System Renderer
         ParticleSystemRenderer psr = GetComponent<ParticleSystemRenderer>();
         //Change the colour
-        psr.material = sprinklecolour[randomColour];
+        psr.material = materials[randomColour];
     }
 
     // Update is called once per frame
@@ -40,22 +35,11 @@ public class ChangeSprinkleColour : MonoBehaviour
     void RandomSprinkle() 
     {
         //Pick a number between 0 and the amount of colours in the Sprinkles Colour List
-        randomColour = Random.Range(0, sprinklecolour.Count);
+        randomColour = Random.Range(0, materials.Count);
         //Pick a number between 0 and the amount of shapes in the list
         randomShape = Random.Range(0, sprinkleShape.Count);
     }
 
-    void AddColoursToDictionary() 
-    {
-        //For each item in the Material list
-        for (int i = 0; i < materials.Count; i++) 
-        {
-            //Add them into the Dictionary for sprinkle colours
-            sprinklecolour.Add(i, materials[i]);
-
-            Debug.Log(materials[i]);
-        }
-    }
 
     //Changes the sprinkle size depending on the shape of the sprinkle
     void ChangeSprinkleSize(ParticleSystem.MainModule psmain, int ShapeOfSprinkle) 
